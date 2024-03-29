1a2dbebd477ccdef958603ceb5196a8434c9f1c8
Julian Caicedo <juccaicedoac@unal.edu.co>
2024-03-29 00:33:34 -0500

    update navbar

--- components/NavBar.js
+++ components/NavBar.js
@@ -4,6 +4,7 @@ import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import styles from '../styles/NavBar.module.css';
 import TranslateIcon from '@mui/icons-material/Translate';
+import CheckIcon from '@mui/icons-material/Check';
 
 const NavBar = () => {
     // const [isOpen, setIsOpen] = useState(false);
@@ -26,47 +27,63 @@ const NavBar = () => {
 
     return (
         <nav className={ `${styles.navbar} ${isScrolled ? styles.navbarScroll : ''}` }>
-            <div className={styles.navigation}>
-                <div>
-                    <Link href="/" className={styles.link}>
-                        LOGO
+            <div className={styles.navContent}>
+                <div className={styles.navigation}>
+                    <div>
+                        <Link href="/" className={styles.link}>
+                            LOGO
+                        </Link>
+                    </div>
+                    <div>
+                        <ul>
+                            <li>
+                                <Link data-scroll="features" href="#features"  className={styles.link}>
+                                    <button className='mainButton'>
+                                        About
+                                    </button>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link data-scroll="pricing" href="#pricing"  className={styles.link}>
+                                    <button className='mainButton'>
+                                        Contact
+                                    </button>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link data-scroll="about" href="#about"  className={styles.link}>
+                                    <button className='mainButton'>
+                                        Login
+                                    </button>
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div className={styles.rightMenu}>
+                    <div className='dropdownContainer'>
+                        <button className='mainButton'>
+                            <TranslateIcon style={{fontSize:'20px'}}  />
+                        </button>
+                        <div className='dropdownMenu'>
+                            <div className='dropdownSelect'>
+                                <button className='mainButtonSelect'>
+                                    English
+                                    <CheckIcon style={{fontSize:'18px', margin:'0px 5px 0px 0px'}} />
+                                </button>
+                                <button className='mainButtonSelect'>
+                                    Spanish
+                                    <CheckIcon style={{fontSize:'18px', margin:'0px 5px 0px 0px'}} />
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <Link href="/login"  className={styles.link}>
+                        <button className='mainButtonColor'>
+                            Login
+                        </button>
                     </Link>
                 </div>
-                <div>
-                    <ul>
-                        <li>
-                            <Link data-scroll="features" href="#features"  className={styles.link}>
-                                <button className='mainButton'>
-                                    About
-                                </button>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link data-scroll="pricing" href="#pricing"  className={styles.link}>
-                                <button className='mainButton'>
-                                    Contact
-                                </button>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link data-scroll="about" href="#about"  className={styles.link}>
-                                <button className='mainButton'>
-                                    Login
-                                </button>
-                            </Link>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <div className={styles.rightMenu}>
-                <button className='mainButton'>
-                    <TranslateIcon style={{fontSize:'20px'}}  />
-                </button>
-                <Link href="/login"  className={styles.link}>
-                    <button className='mainButtonColor'>
-                        Login
-                    </button>
-                </Link>
             </div>
         </nav>
     );
